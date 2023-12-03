f30367779b871e92fbc35fd16db9c3499c888837
saparashar <saparashar@microsoft.com>
2023-12-03 14:33:21 -0800

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@/blockchain/ergo/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
