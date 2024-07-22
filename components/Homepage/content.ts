f479545cb6f6615f12e4a4e494f4fe207dc58109
Bruno <bruno.wp@gmail.com>
2024-07-22 08:41:46 +0300

    Update content.ts

--- components/Homepage/content.ts
+++ components/Homepage/content.ts
@@ -1,27 +1,18 @@
 export const callOutTitle =
-  "Novel Stabilization Protocol Inspired by Nuclear Physics";
+  "Gluon-Based Gold Stablecoins on Ergo";
 export const callOutDesc =
-  "Gluon is a new Stabilization protocol based on Ergo chain, offering secure stability and yielding volatility based on your needs.";
-export const infoTitleGAU = "Hedge Against Market Volatility with $GAU";
-export const infoDescGAU =
-  "Convert your ERG into GAU, Gluon’s stable neutral tokeon and protect your funds from the volatility of the markets";
-export const infoTitleGAUC =
-  "Leverage Volatility and Increase Revenue with $GAUC";
-export const infoDescGAUC =
-  "Convert your ERG into GAUC, Gluon’s volatile tokeon";
-
+  "Gluon is a novel stabilization protocol inspired by nuclear physics, offering reliable stability or exposure to volatility based on your needs. Have digital Gold on-chain, backed by ERG, without the risks and inconveniences of physical gold.";
+export const infoTitleGAU = "Protect your Wealth from Market Volatility with $GAU";
+export const infoDescGAU = "Convert your ERG into GAU, ERG’s stable tokeon. Every GAU is pegged to 1 gram of Gold.";
+export const infoTitleGAUC = "Leverage Volatility and Get Yield with $GAUC";
+export const infoDescGAUC = "Convert your ERG into GAUC, ERG’s volatile tokeon. Every GAUC gives leveraged exposure to ERG and protocol fees";
 export const mechanicsTitle = "Gluon Mechanics";
-export const mechanicsDesc =
-  "Inspired by nuclear physics and similar to how an atom nucleus is comprised of Protons and Neutrons, the base token on Gluon, Ergo, is also comprised of GAU and GAUC. The following reactions are fundamental functions of how Gluon works:";
+export const mechanicsDesc = "As an atomic nucleus is comprised of protons and neutrons, ERG is seen by Gluon as comprised of volatile tokeons (GAUC) and stable tokeons (GAU). By interacting with Gluon's reactor, users can perform the following fundamental actions inspired by nuclear reactions:";
 export const mechanicsFissionTitle = "Fission";
-export const mechanicsFissionDesc =
-  "Splits $ERG tokens into $GAU stablecoins and $GAUC reservecoins";
+export const mechanicsFissionDesc = "Splits $ERG tokens into $GAU stable tokeons and $GAUC volatile tokeons";
 export const mechanicsFusionTitle = "Fusion";
-export const mechanicsFusionDesc =
-  "Merges $GAU stablecoins and $GAUC reservecoins into $ERG tokens";
-export const mechanicsBetaDecayPlusTitle = "BetaDecay+";
-export const mechanicsBetaDecayPlusDesc =
-  "Transforms $GAUC reservecoins into $GAU stablecoins";
-export const mechanicsBetaDecayMinusTitle = "BetaDecay-";
-export const mechanicsBetaDecayMinusDesc =
-  "Transforms $GAU stablecoins into $GAUC reservecoins";
+export const mechanicsFusionDesc = "Merges $GAU stable tokeons and $GAUC volatile tokeons into $ERG tokens";
+export const mechanicsBetaDecayPlusTitle = "BetaDecay of GAUC";
+export const mechanicsBetaDecayPlusDesc = "Transmutes $GAUC volatile tokeons into $GAU stable tokeons";
+export const mechanicsBetaDecayMinusTitle = "BetaDecay of GAU";
+export const mechanicsBetaDecayMinusDesc = "Transmutes $GAU stable tokeons into $GAUC volatile tokeons";
